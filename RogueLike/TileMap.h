f91be8e7e8206f2ab0518776016571f490a7d6c0
Oliver Pöckelhofer <oliver.poeckelhofer@student.tugraz.at>
2017-06-26 12:48:34 +0200

    Edit .gitignore

---
+++ RogueLike/TileMap.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include "Room.h"
+#include "Types.h"
+
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+private:
+	sf::Texture tileset_;
+	sf::VertexArray vertices_;
+
+
+	virtual void draw(sf::RenderTarget& target, 
+					  sf::RenderStates states) const;
+
+public:
+	TileMap();
+	bool load(const char* filename, const Map& map, 
+			  int tile_size, int width, int height);
+};
